5a93e036edb07c415a14c3dce451cee4a936080e
Haithem Turki <hturki@cs.cmu.edu>
2023-12-07 16:37:06 -0500

    poster attempt 2

--- resources/handler.js
+++ resources/handler.js
@@ -5,8 +5,8 @@ const mic = document.getElementById("mic");
 const ship = document.getElementById("ship");
 
 ficus.addEventListener("click", function(e){
-  multiscaleVideo.src="./vids/ficus.mp4";
-  multiscaleVideo.poster="./images/ficus.jpg";
+  multiscaleVideo.src = "./vids/ficus.mp4";
+  multiscaleVideo.poster = "./images/ficus.jpg";
   ficus.className = "selected-scene";
   lego.className = "";
   mic.className = "";
@@ -14,8 +14,8 @@ ficus.addEventListener("click", function(e){
 });
 
 lego.addEventListener("click", function(e){
-  multiscaleVideo.src="./vids/lego.mp4";
-  multiscaleVideo.poster="./images/lego.jpg";
+  multiscaleVideo.src = "./vids/lego.mp4";
+  multiscaleVideo.poster = "./images/lego.jpg";
   ficus.className = "";
   lego.className = "selected-scene";
   mic.className = "";
@@ -23,8 +23,8 @@ lego.addEventListener("click", function(e){
 });
 
 mic.addEventListener("click", function(e){
-  multiscaleVideo.src="./vids/mic.mp4";
-  multiscaleVideo.poster="./images/mic.jpg";
+  multiscaleVideo.src = "./vids/mic.mp4";
+  multiscaleVideo.poster = "./images/mic.jpg";
   ficus.className = "";
   lego.className = "";
   mic.className = "selected-scene";
@@ -32,47 +32,52 @@ mic.addEventListener("click", function(e){
 });
 
 ship.addEventListener("click", function(e){
-  multiscaleVideo.src="./vids/ship.mp4";
-  multiscaleVideo.poster="./images/ship.jpg";
+  multiscaleVideo.src = "./vids/ship.mp4";
+  multiscaleVideo.poster = "./images/ship.jpg";
   ficus.className = "";
   lego.className = "";
   mic.className = "";
   ship.className = "selected-scene";
 });
 
+multiscaleVideo.poster = "./images/ficus.jpg";
 
 const blenderAVideo = document.getElementById("blender-a-video");
 const checkerboard = document.getElementById("checkerboard");
 const brick = document.getElementById("brick");
 
 checkerboard.addEventListener("click", function(e){
-  blenderAVideo.src="./vids/checkerboard.mp4";
-  blenderAVideo.poster="./vids/checkerboard.jpg";
+  blenderAVideo.src = "./vids/checkerboard.mp4";
+  blenderAVideo.poster = "./vids/checkerboard.jpg";
   checkerboard.className = "selected-scene";
   brick.className = "";
 });
 
 brick.addEventListener("click", function(e){
-  blenderAVideo.src="./vids/brick.mp4";
-  blenderAVideo.src="./vids/brick.mp4";
+  blenderAVideo.src = "./vids/brick.mp4";
+  blenderAVideo.src =  " ./vids/brick.mp4";
   checkerboard.className = "";
   brick.className = "selected-scene";
 });
+
+blenderAVideo.poster = "./vids/checkerboard.jpg";
 
 const boatVideo = document.getElementById("boat-video");
 const boatNearest = document.getElementById("boat-nearest");
 const boatComp = document.getElementById("boat-comp");
 
 boatNearest.addEventListener("click", function(e){
-  boatVideo.src="./vids/boat-nearest.mp4";
-  boatVideo.poster="./vids/boat-nearest.jpg";
+  boatVideo.src = "./vids/boat-nearest.mp4";
+  boatVideo.poster = "./vids/boat-nearest.jpg";
   boatNearest.className = "selected-scene";
   boatComp.className = "";
 });
 
 boatComp.addEventListener("click", function(e){
-  boatVideo.src="./vids/boat-comp.mp4";
-  boatVideo.poster="./vids/boat-comp.jpg";
+  boatVideo.src = "./vids/boat-comp.mp4";
+  boatVideo.poster = "./vids/boat-comp.jpg";
   boatNearest.className = "";
   boatComp.className = "selected-scene";
 });
+
+boatVideo.poster = "./vids/boat-nearest.jpg";
